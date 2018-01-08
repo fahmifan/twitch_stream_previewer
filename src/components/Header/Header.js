2253cfa85f01b1f3e82f0e0eeae0ecdfb260caef
fahmi-irfan <fahmiirfan909@gmail.com>
2018-01-08 20:12:04 +0700

    Add new Header Comps

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 export default () => (
-  <header className="bg-white black-80 tc pv4 avenir">
-    <h1 className="mt2 mb0 baskerville i fw1 f1">Streamers</h1>
-    <h2 className="mt2 mb0 f6 fw4 ttu tracked">Twitch User Previewer</h2>
-    <nav className="bt bb tc mw7 center mt4">
-      <a className="f6 f5-l link bg-animate black-80 hover-bg-light-blue dib pa3 ph4-l" href="/shop">All</a>
-      <a className="f6 f5-l link bg-animate black-80 hover-bg-light-pink dib pa3 ph4-l" href="/about">Online</a>
-      <a className="f6 f5-l link bg-animate black-80 hover-bg-light-yellow dib pa3 ph4-l" href="/contact">Offline</a>
+  <header className="">
+    <nav className="helvetica fl w-100 white shadow-3 bg-light-green">
+      <ul className="list h-auto pa0 flex items-center justify-around">
+        <li className="dib v-top pa2 link dim black b pointer tc">All</li>
+        <li className="dib v-top pa2 link dim black b pointer tc ">Online</li>
+        <li className="dib v-top pa2 link dim black b pointer tc ">Offline</li>
+      </ul>
     </nav>
   </header>
 );
