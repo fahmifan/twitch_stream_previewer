78b5104ab7315a8536a887faadf7528ca825d19c
fahmi-irfan <fahmiirfan909@gmail.com>
2018-01-09 20:20:59 +0700

    Add shadow box

--- src/components/User/DisplayPic/DisplayPic.js
+++ src/components/User/DisplayPic/DisplayPic.js
@@ -4,7 +4,7 @@ export default (props) => {
   return (
     <div className="ph2 m-25">
       <img
-        className="br-100 ba h3 w3" 
+        className="br-100 ba h3 w3 shadow-4" 
         src={props.src} 
         alt="steramer"/>
     </div>
