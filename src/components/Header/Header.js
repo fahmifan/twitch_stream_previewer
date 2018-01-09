6b2aa0ab9b7b7b70bac392cfd116947c0eec4c0a
fahmi-irfan <fahmiirfan909@gmail.com>
2018-01-09 19:50:45 +0700

    replace li with NavItem

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
+import NavItem from './NavItem/NavItem';
+
 export default () => (
   <header className="">
-    <nav className="helvetica fl w-100 white shadow-3 bg-light-green">
+    <nav className="helvetica fl w-100 white shadow-3 bg-light-green mb4">
       <ul className="list h-auto pa0 flex items-center justify-around">
-        <li className="dib v-top pa2 link dim black b pointer tc">All</li>
-        <li className="dib v-top pa2 link dim black b pointer tc ">Online</li>
-        <li className="dib v-top pa2 link dim black b pointer tc ">Offline</li>
+        <NavItem>All</NavItem>
+        <NavItem>Online</NavItem>
+        <NavItem>Offline</NavItem>
       </ul>
     </nav>
   </header>
