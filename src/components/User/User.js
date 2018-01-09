6831bbfed62a967e6c9889c0dbb38a1c7668fccc
fahmi-irfan <fahmiirfan909@gmail.com>
2018-01-09 19:31:01 +0700

    Add User component

---
+++ src/components/User/User.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import DisplayPic from './DisplayPic/DisplayPic';
+import DescCard from './DescCard/DescCard';
+import pic from '../../logo.svg';
+
+export default () => (
+  <div className="w-100 flex items-center">
+      <DisplayPic src={pic} />
+      <DescCard />
+  </div>
+);
