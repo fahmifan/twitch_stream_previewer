549b24a95cb6e24475b962e5d906e4947a8943b6
fahmi-irfan <fahmiirfan909@gmail.com>
2018-01-09 19:30:37 +0700

    Add DescCard component

---
+++ src/components/User/DescCard/DescCard.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default (props) => (
+  <div className="ba h3 w-75 mv3 mr2">
+    <div className="flex items-center flex-column pt2">
+      <p className="ma0 tc">React</p>
+      <p className="ma0 tc">Js Library for UI</p>
+    </div>
+  </div>
+);
