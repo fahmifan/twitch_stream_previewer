cde10c25d68a64f162c70bede13c7e88a8aeeb3b
fahmi-irfan <fahmiirfan909@gmail.com>
2018-01-09 19:52:58 +0700

    remove border

--- src/components/User/DescCard/DescCard.js
+++ src/components/User/DescCard/DescCard.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export default (props) => (
-  <div className="ba h3 w-75 mv3 mr2">
+  <div className="h3 w-75 mr2">
     <div className="flex items-center flex-column pt2">
       <p className="ma0 tc">React</p>
       <p className="ma0 tc">Js Library for UI</p>
