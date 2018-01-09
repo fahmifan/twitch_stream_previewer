c2e67eac9fb4d707cc267947f1c56753fa56804c
fahmi-irfan <fahmiirfan909@gmail.com>
2018-01-09 18:06:52 +0700

    delete navitem to fix error navitem undef

--- src/components/Header/NavItem/NavItem.js
+++ src/components/Header/NavItem/NavItem.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default navitem = (props) => (
+export default (props) => (
   <li className="dib v-top pa2 link dim black b pointer tc">
     {props.children}
   </li>
