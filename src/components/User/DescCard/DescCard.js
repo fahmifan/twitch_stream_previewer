9e349955f785830bb1f24ebb78204b5f75f93ddc
fahmi-irfan <fahmiirfan909@gmail.com>
2018-01-09 20:20:39 +0700

    change font to helvetica

--- src/components/User/DescCard/DescCard.js
+++ src/components/User/DescCard/DescCard.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default (props) => (
   <div className="h3 w-75 mr2">
-    <div className="flex items-center flex-column pt2">
+    <div className="flex items-center flex-column pt2 helvetica">
       <p className="ma0 tc">React</p>
       <p className="ma0 tc">Js Library for UI</p>
     </div>
