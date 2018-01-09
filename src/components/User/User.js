6e921596fb5c4923e80fd78eb330e08b5473a71c
fahmi-irfan <fahmiirfan909@gmail.com>
2018-01-09 19:50:23 +0700

    Add margin bottom

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -5,7 +5,7 @@ import DescCard from './DescCard/DescCard';
 import pic from '../../logo.svg';
 
 export default () => (
-  <div className="w-100 flex items-center">
+  <div className="w-100 flex items-center mb3">
       <DisplayPic src={pic} />
       <DescCard />
   </div>
