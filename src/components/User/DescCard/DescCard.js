ddea3011111b7a2f12cac0557bd68fba746546a1
fahmi-irfan <fahmiirfan909@gmail.com>
2018-01-12 22:30:57 +0700

    Add element from props

--- src/components/User/DescCard/DescCard.js
+++ src/components/User/DescCard/DescCard.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
-export default (props) => (
+const descCard = (props) => (
   <div className="h3 w-75 mr2">
-    <div className="flex items-center flex-column pt2 helvetica">
-      <p className="ma0 tc">React</p>
-      <p className="ma0 tc">Js Library for UI</p>
+    <div className="flex items-center flex-column pt2 helvetica h-100">
+      <p className="ma0 tc b"><a 
+        href={props.url}
+        className="link black dim pointer"
+        >{props.display_name}</a></p>
+      <p className="ma0 tc i">{props.isOnline ? props.game : 'Offline'}</p>
+      <p className="ma0 h-100 overflow-x-auto pa2 tc">{props.status}</p>
     </div>
   </div>
 );
+
+export default descCard;
