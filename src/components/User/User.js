6867c49bbc8b446b1bf8fb5fc45830b9bb11a847
fahmi-irfan <fahmiirfan909@gmail.com>
2018-01-12 22:28:55 +0700

    Add new state

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -3,7 +3,12 @@ import axios from 'axios';
 
 import DisplayPic from './DisplayPic/DisplayPic';
 import DescCard from './DescCard/DescCard';
-import pic from '../../logo.svg';
+
+class User extends Component {
+  state = {
+    streamData: {},
+  }
+
 
 export default () => (
   <div className="w-90 pa2 flex flex-row items-center mb1 br3 shadow-4 bg-light-green">
