44ee255825c9729a9f189536baf8b80c4ae655a5
fahmi-irfan <fahmiirfan909@gmail.com>
2018-01-12 22:29:20 +0700

    Add ajax request using axios

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -9,8 +9,23 @@ class User extends Component {
     streamData: {},
   }
 
+  componentWillMount() {
+    const username = this.props.username;
 
-export default () => (
+    this.getUserData(username, 'streams')
+      .then(res => {
+        this.setState({
+          streamData: res.data
+        })
+      })  
+  }
+
+  getUserData = (username, type) => {
+    const base_url = "https://wind-bow.glitch.me/twitch-api/";
+    return axios.get(`${base_url}/${type}/${username}`);
+  }
+
+  render() {
   <div className="w-90 pa2 flex flex-row items-center mb1 br3 shadow-4 bg-light-green">
       <DisplayPic src={pic} />
       <DescCard />
