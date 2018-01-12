cd7c0a452cbd083115ccbaa6285b59136021074d
fahmi-irfan <fahmiirfan909@gmail.com>
2018-01-12 22:25:53 +0700

    Add ajax request using axios

--- src/containers/Users/Users.js
+++ src/containers/Users/Users.js
@@ -19,5 +19,28 @@ class Users extends Component {
     isLoading: false,
   }
 
+  componentWillMount() {
+    this.usersData();
+  }
+
+  usersData = () => {
+    this.state.users.forEach(user => {
+      this.getUserData(user, 'users')
+        .then(res => {
+          let usersData = [...this.state.usersData];
+          usersData.push(res.data);
+          this.setState({usersData: usersData});
+        })
+    })
+  }
+
+  /**
+   * Get a user data from a endpoint /:type/:user 
+   * @param type : string['channels', 'streams', 'users']
+   */
+  getUserData = (user, type) => {
+    const base_url = "https://wind-bow.glitch.me/twitch-api/";
+    return axios.get(`${base_url}/${type}/${user}`);
+  }
   </div>
 );
