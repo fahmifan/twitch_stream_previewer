5dcb41bf00bac9e5836536101ac2379fead3adf0
fahmi-irfan <fahmiirfan909@gmail.com>
2018-01-13 07:33:19 +0700

    change NavItem.children to Twitcher

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,10 +5,8 @@ import NavItem from './NavItem/NavItem';
 export default () => (
   <header className="">
     <nav className="helvetica fl w-100 white shadow-3 bg-light-green mb4">
-      <ul className="list h-auto pa0 flex items-center justify-around">
-        <NavItem>All</NavItem>
-        <NavItem>Online</NavItem>
-        <NavItem>Offline</NavItem>
+      <ul className="list h-auto ma0 pa0 flex items-center justify-around">
+        <NavItem>Twitcher</NavItem>
       </ul>
     </nav>
   </header>
