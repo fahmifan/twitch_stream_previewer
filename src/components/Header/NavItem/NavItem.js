c8d6ebb6a00ce2f85acbb2295c3f4360ec54a72e
fahmi-irfan <fahmiirfan909@gmail.com>
2018-01-13 07:33:43 +0700

    customize NavItem

--- src/components/Header/NavItem/NavItem.js
+++ src/components/Header/NavItem/NavItem.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 export default (props) => (
-  <li className="dib v-top pa2 link dim black b pointer tc">
-    {props.children}
+  <li className="dib v-top pv1 link dim black b pointer center tc f1 font-vt323">
+    <a href="https://www.twitch.tv/" className="link black">
+      {props.children}
+    </a>
   </li>
 );
